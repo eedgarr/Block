6748fe1d1a9ee70547f1fff160867ffe8b9bc786
eedgarr <bord0913@naver.com>
2018-04-24 20:31:13 +0900

    Revert "Revert "Revert "Revert "class란?""""
    This reverts commit 7863db9338d76b5f66e98f38658ecac7496aaedc.

---
+++ chain/puppy.rb
@@ -0,0 +1,26 @@
+#클래스란 무엇인가
+
+class Puppy
+	def initialize
+	 	@hungry = true
+	end
+
+	def feed
+		puts "배불러요"
+	end
+
+	def run
+		puts "다리아파요"
+	end
+
+	def coding
+		puts "오!"
+	end
+	
+	def jump
+		puts "점프!"
+	end
+end
+
+aaaaaaaaaaaaaa = Puppy.new
+aaaaaaaaaaaaaa.run
