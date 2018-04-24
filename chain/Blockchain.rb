58470f32973dccea6b91028307b187d3fb76d6e1
eedgarr <bord0913@naver.com>
2018-04-24 20:30:48 +0900

    Revert "Revert "class란?""
    This reverts commit 7860757d3e3dfadb03532655abbd810ad4641d71.

---
+++ chain/Blockchain.rb
@@ -0,0 +1,26 @@
+
+class BlockChain
+	def initialize
+	end
+
+	def make_genesis_block
+		puts "1"
+	end
+
+	def mining
+		puts ""
+	end
+
+	def transaction
+		puts ""
+	end
+	
+	def connect_blocks
+		puts ""
+	end
+end
+
+aaaaaaaaaaaaaa = BlockChain.new
+aaaaaaaaaaaaaa.make_genesis_block
+
+
