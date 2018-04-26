b81269e43ee37ea93dbec07ea5c1c7cfd212f0ac
eedgarr <bord0913@naver.com>
2018-04-26 23:07:14 +0900

    Yes!

--- chain/server.rb
+++ chain/server.rb
@@ -4,10 +4,20 @@ require './block'
 b = BlockChain.new
 
 get '/' do
-  b.current_chain.to_s
+	message = ""
+  	b.current_chain.each do |c|
+  	message << "<hr>"
+  	message << "블록 번호는 " + c['index'].to_s + "번 입니다.<br>"
+  	message << "Nonce는 " + c['nonce'].to_s + "입니다.<br>"
+  	message << "시간은 " + c['time'].to_s + " 입니다.<br>"
+  	message << "앞 주소는 " + c['previous_block'].to_s + " 입니다.<br>"
+  	message << "나의 주소는 " + Digest::SHA256.hexdigest(c.to_s) + " 입니다.<br>"
+	message << "<hr>"
+	end
+	message
 end
 
 get '/mine' do
 	b.mining
-   "Mined"
+   "Mined!!"
 end
