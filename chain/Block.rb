bbf2054b11038f7419165bb25d1cfcbb58a59a88
eedgarr <bord0913@naver.com>
2018-04-30 20:23:33 +0900

    data

--- chain/Block.rb
+++ chain/Block.rb
@@ -1,13 +1,22 @@
+@@ -1,23 +1,31 @@
 require 'digest'
 
 class BlockChain
 
 	def initialize
+		@number_of_blocks = 1
 		@chain = []
 	end
 
 	def mining
+		@number_of_blocks = @number_of_blocks + 1
+		history = []
 		begin
+			nonce = rand(10000000)
+			hashed = Digest::SHA256.hexdigest(nonce.to_s)
+			history << nonce
+		end while hashed[0..4] != '00000'
+		history
 			nonce = rand(100)
 		end while nonce != 0
 
@@ -24,9 +33,9 @@ class BlockChain
 		@chain
 	end
 
+	def my_blocks
+		@number_of_blocks
 	def last_block
 		@chain[-1]
 	end
 end
-
-
